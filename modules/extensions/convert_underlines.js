80d14b8d4d318e3c7cb196a94ef042c15ddb0687
Sandeep Rudra <sandeeprudra30@gmail.com>
2025-05-28 18:25:09 +0200

    updated the convert file

--- modules/extensions/convert_underlines.js
+++ modules/extensions/convert_underlines.js
@@ -1,14 +1,15 @@
+// modules/extensions/convert_underlines.js
 'use strict'
 
 module.exports.register = function (registry) {
-  registry.preprocessor(function () {
-    const lines = this.reader.getLines().map((line) =>
+  registry.preprocessor(function (doc, reader) {
+    const lines = reader.getLines().map((line) =>
       line
         // convert any raw HTML underline spans
         .replace(/<span class="underline">([^<]*)<\/span>/g, '[underline]#$1#')
         // convert any pass:q[[underline]#…#]] wrappers
         .replace(/pass:q\[\[underline\]#([^#]+)#\]\]/g, '[role="underline"]#$1#')
     )
-    this.reader.setLines(lines)
+    reader.setLines(lines)
   })
 }
